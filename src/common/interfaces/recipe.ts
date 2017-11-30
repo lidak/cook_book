0b9fa82fbeb2163a5714ed4ca2a399f80acb77e8
Maksim Sokolovsky <msokolovskyi@griddynamics.com>
2017-11-29 21:51:19 -0500

    [core] add some basic functionality

--- src/common/interfaces/recipe.ts
+++ src/common/interfaces/recipe.ts
@@ -1,7 +1,10 @@
 import recipeStep from './recipeStep';
+import moduleInterface from './module';
+import Ingredient from '../../ingredients/ingredient';
 
 export default interface recipeInerface {
     title: string,
     availableOptions:object,
     buildReceipSequence(options: any): [object]
+    isAvailable(modules:moduleInterface[], ingredients:Ingredient[])
 }
